58832244b949bcef9f7ae91e3c62c8f2c9df3db6
aaaa <aaaa@gmail.com>
2026-01-26 15:20:17 +0900

    v0.0

---
+++ src/main/java/com/example/app/TokyoDisasterRiskVisualizationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokyoDisasterRiskVisualizationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokyoDisasterRiskVisualizationAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/app/utility/MeshAggregationUtil.java
@@ -0,0 +1,5 @@
+package com.example.app.utility;
+
+public class MeshAggregationUtil {
+
+}
